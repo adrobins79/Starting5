5a82451f51f3469b19fd99637a46a50a7ed3c3bd
ADRIAN <adrian@nowhere.com>
2015-01-12 19:13:55 -0500

    changed to long

--- app/src/main/java/com/example/adrian/leagueplayers/activity/GetScheduleTask.java
+++ app/src/main/java/com/example/adrian/leagueplayers/activity/GetScheduleTask.java
@@ -28,7 +28,7 @@ import java.util.Date;
 /**
  * Async task class to get json by making HTTP call
  * */
-public class GetScheduleTask extends AsyncTask<Void, Void, String>{
+public class GetScheduleTask extends AsyncTask<Void, Void, ArrayList<Map<String, String>>>{
     private ListActivity activity;
     private String url = "http://mi.nba.com/statsm2/scores/miniscoreboard.json";
     public GetScheduleTask(ListActivity activity){
@@ -41,7 +41,8 @@ public class GetScheduleTask extends AsyncTask<Void, Void, String>{
     }
 
     @Override
-    protected String doInBackground(Void... arg0) {
+    protected ArrayList<Map<String, String>> doInBackground(Void... arg0) {
+        ArrayList<Map<String, String>> dataList = new ArrayList<Map<String, String>>();
         String todayUtc  = null;
         HttpService http = new HttpService();
 
@@ -51,28 +52,53 @@ public class GetScheduleTask extends AsyncTask<Void, Void, String>{
 
             JSONObject payload = json.getJSONObject("payload");
             JSONObject today = payload.getJSONObject("today");
+            JSONArray games = today.getJSONArray("games");
             todayUtc = today.getString("utcMillis");
+            TextView scheduleDate = (TextView)activity.findViewById(R.id.scheduleDate);
+            Calendar utcCal= Calendar.getInstance();
+            utcCal.setTimeInMillis(Long.valueOf(todayUtc));
+
+            SimpleDateFormat sdf= new SimpleDateFormat("EEE, MMM dd yyyy");
+            Date utcDate= utcCal.getTime();
+            String formattedDate = sdf.format(utcDate);
+
+            scheduleDate.setText(formattedDate);
+            for(int i=0; i < games.length(); i++){
+                JSONObject homeTeam = games.getJSONObject(i).getJSONObject("homeTeam");
+                JSONObject homeProfile = homeTeam.getJSONObject("profile");
+
+                JSONObject awayTeam = games.getJSONObject(i).getJSONObject("awayTeam");
+                JSONObject awayProfile = awayTeam.getJSONObject("profile");
+
+                HashMap<String, String> map = new HashMap<String, String>();
+                map.put("date", formattedDate);
+                map.put("homeTeamName", homeProfile.getString("city") + " " + homeProfile.getString("name"));
+                map.put("awayTeamName", awayProfile.getString("city") + " " + awayProfile.getString("name"));
+                dataList.add(map);
+            }
 
         } catch (JSONException e) {
             e.printStackTrace();
         } catch (ServiceException e) {
             e.printStackTrace();
         }
-        return todayUtc;
+        return dataList;
     }
 
     @Override
-    protected void onPostExecute(String todayUtc) {
-        super.onPostExecute(todayUtc);
-        TextView scheduleDate = (TextView)activity.findViewById(R.id.scheduleDate);
-        Calendar utcCal= Calendar.getInstance();
-        utcCal.setTimeInMillis(Long.valueOf(todayUtc));
+    protected void onPostExecute(ArrayList<Map<String, String>> dataList) {
+        super.onPostExecute(dataList);
+        /**
+         * Updating parsed JSON data into ListView
+         * */
+        ListAdapter adapter = new SimpleAdapter(
+                activity,
+                dataList,
+                R.layout.list_item,
+                new String[] {"homeTeamName"},
+                new int[] { R.id.name});
 
-        SimpleDateFormat sdf= new SimpleDateFormat("EEE, MMM dd yyyy");
-        Date utcDate= utcCal.getTime();
-        String formattedDate = sdf.format(utcDate);
-
-        scheduleDate.setText(formattedDate);
+        activity.setListAdapter(adapter);
     }
 
 }
